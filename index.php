7415db98b445fc08a10e2728873b8c6a986a17f7
khoirul94 <39944640+khoirul94@users.noreply.github.com>
2018-09-13 06:20:27 +0700

    sss

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ require_once "config/database.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Aplikasi CRUD</title>
+    <title>Aplikasi CRUD ssss</title>
     
     <!-- favicon -->
     <link rel="shortcut icon" href="assets/img/favicon.png">
